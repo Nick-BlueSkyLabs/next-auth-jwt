141c67d2feec11bba1cc3af2c05e7909a7d6573e
Nick Wylynko <nick@blueskylabs.com.au>
2021-08-13 17:36:28 +0800

    add firebase 9

---
+++ Firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyCMF1e39Lu17xZ1GkekQQ5CiWx3pxCMROU",
+  authDomain: "nick-learning-322807.firebaseapp.com",
+  projectId: "nick-learning-322807",
+  storageBucket: "nick-learning-322807.appspot.com",
+  messagingSenderId: "154060874241",
+  appId: "1:154060874241:web:0659b3c9f9d9e13e825600"
+};
